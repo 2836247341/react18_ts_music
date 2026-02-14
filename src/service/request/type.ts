f78195d2a98b89860368c163f347ea4ff5dd9cbe
乌云车站 <2836247341@qq.com>
2026-02-14 11:47:44 +0800

    axios二次封装

---
+++ src/service/request/type.ts
@@ -0,0 +1,13 @@
+import type { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+export interface HYInterceptors<T = AxiosResponse> {
+  requestSuccessFn?: (config: AxiosRequestConfig) => AxiosRequestConfig
+  requestFailureFn?: (err: any) => any
+  responseSuccessFn?: (res: T) => T
+  responseFailureFn?: (err: any) => any
+}
+
+export interface HYRequestConfig<T = AxiosResponse> extends AxiosRequestConfig {
+  // 在 AxiosRuequestConfig 的基础上添加一个 interceptor
+  interceptor?: HYInterceptors<T>
+}
